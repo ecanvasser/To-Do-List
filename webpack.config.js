9e9cf8567be5b1f1e300b2fe61ef70547b5094d6
ecanvasser <eddiecanvassere9@gmail.com>
2022-11-21 12:11:47 -0800

    Create additional modules and add css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    project: './src/project.js',
+    to_do: './src/to-do.js'
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
