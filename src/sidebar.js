7a83d0862bc9a3551336db437d870dcb66e673dc
ecanvasser <eddiecanvassere9@gmail.com>
2022-11-25 22:22:33 -0800

    Add event listener to sidebar a tag for to-do view

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,5 +1,6 @@
 export {Sidebar}
 import Arrow from './arrow.svg'
+import { toDoView } from './to-do.js'
 
 const Sidebar = () => {
 
@@ -11,6 +12,7 @@ const Sidebar = () => {
             var sbTitle = array[i].title;
             let sbDiv = sbcol.insertAdjacentElement('beforeend', document.createElement('div'));
             sbDiv.setAttribute('class', 'sb-row');
+            sbDiv.setAttribute('id', `${sbTitle}`);
             sbDiv.innerHTML = sbTitle;
             setLink(sbDiv);
         }
@@ -23,17 +25,24 @@ const Sidebar = () => {
         let sbaTag = div.insertAdjacentElement('beforeend', document.createElement('a'));
         sbaTag.setAttribute('class', 'sb-link');
         sbaTag.setAttribute('href', "");
-        linkEvent(sbaTag);
+        sbaTag.addEventListener('click', (e) => {
+            let tdText = e.target.closest('.sb-row').id;
+            let tdLink = toDoView();
+            tdLink.build(tdText);
+            e.preventDefault();
+        })
 
         let sbImg = sbaTag.insertAdjacentElement('beforeend', projArrow);
         sbImg.setAttribute('class', 'sb-img');
     }
 
-    const linkEvent = (link) => {
-        link.addEventListener('click', () => {
-            console.log('test')
-        });
-    }
+    // const linkEvent = (link, e) => {
+    //     link.addEventListener('click', () => {
+    //         console.log('test');
+    //         // e.preventDefault();
+    //     });
+    //     e.preventDefault();
+    // }
 
     return {
         addProj
