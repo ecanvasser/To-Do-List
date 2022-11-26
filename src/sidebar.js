c1b6f16aba1cbc2d311c8829fa466e5c7308d18b
ecanvasser <eddiecanvassere9@gmail.com>
2022-11-25 17:37:33 -0800

    Add arrow icon to sidebar elements

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,4 +1,5 @@
 export {Sidebar}
+import Arrow from './arrow.svg'
 
 const Sidebar = () => {
 
@@ -11,7 +12,27 @@ const Sidebar = () => {
             let sbDiv = sbcol.insertAdjacentElement('beforeend', document.createElement('div'));
             sbDiv.setAttribute('class', 'sb-row');
             sbDiv.innerHTML = sbTitle;
+            setLink(sbDiv);
         }
+    }
+
+    const setLink = (div) => {
+        const projArrow = new Image();
+        projArrow.src = Arrow;
+
+        let sbaTag = div.insertAdjacentElement('beforeend', document.createElement('a'));
+        sbaTag.setAttribute('class', 'sb-link');
+        sbaTag.setAttribute('href', "");
+        linkEvent(sbaTag);
+
+        let sbImg = sbaTag.insertAdjacentElement('beforeend', projArrow);
+        sbImg.setAttribute('class', 'sb-img');
+    }
+
+    const linkEvent = (link) => {
+        link.addEventListener('click', () => {
+            console.log('test')
+        });
     }
 
     return {
